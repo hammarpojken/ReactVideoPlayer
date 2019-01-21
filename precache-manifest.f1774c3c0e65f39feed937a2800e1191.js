4ac08986bf4381f04a550dd92839a7ced7a9af92
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-21 13:04:11 +0100

    Updates

---
+++ precache-manifest.f1774c3c0e65f39feed937a2800e1191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd49206d097d1a175",
+    "url": "/hammarpojken/ReactVideoPlayer/static/js/runtime~main.ee7cd492.js"
+  },
+  {
+    "revision": "219f5193f211878be18c",
+    "url": "/hammarpojken/ReactVideoPlayer/static/js/main.219f5193.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/hammarpojken/ReactVideoPlayer/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "219f5193f211878be18c",
+    "url": "/hammarpojken/ReactVideoPlayer/static/css/main.b9cd1c21.chunk.css"
+  },
+  {
+    "revision": "d618d68e545da8b1164a840c89c8422f",
+    "url": "/hammarpojken/ReactVideoPlayer/index.html"
+  }
+];
